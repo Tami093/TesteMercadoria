4a66b450936fcc30685e454188efa204a8de3b1b
Tami093 <crisoliveira.93@gmail.com>
2016-10-10 19:56:52 -0300

    Mensagens de erros
    alteração na mensagem de erros

--- app/conexao.php
+++ app/conexao.php
@@ -1,15 +1,16 @@
 
 <?php
-
-
 $servidor = "localhost";
 $usuario = "root";
 $senha = "";
 $banco = "testeonline";
+
+// string de conexão com bd
 $conn = mysqli_connect($servidor,$usuario,$senha,$banco);
 
+// verifica se a conexão com banco de dados 
 if(!$conn) { 
-	echo "Error: Unable to connect to MySQL." . PHP_EOL;
+	echo "Error: Não possível conecta-se ao MySQL." . PHP_EOL;
 	echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
 	echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
 	exit;
